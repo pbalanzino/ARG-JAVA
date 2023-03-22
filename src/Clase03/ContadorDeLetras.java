ac8e65d2cdf49ae3bbc132f685043adbb9e02ba4
Pablo Ezequiel Balanzino <pbalanzino@gmail.com>
2023-03-21 23:54:56 -0300

    Contador de letras

---
+++ src/Clase03/ContadorDeLetras.java
@@ -0,0 +1,20 @@
+package Clase03;
+
+public class ContadorDeLetras {
+    public static void contador(char letra, String palabra){
+        int count = 0;
+        for (int i = 0; i < palabra.length(); i++) {
+            if (letra == palabra.charAt(i)) {
+                count++;
+            }
+        }
+        System.out.println("EJERCICIO 1.a\n");
+        if (count > 1) {
+            System.out.println("La letra " + letra + " esta " + count + " veces en " + palabra);
+        } else if(count == 1) {
+            System.out.println("La letra " + letra + " esta " + count + " vez en " + palabra);
+        } else {
+            System.out.println("La letra " + letra + " no esta en " + palabra);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Clase01.Ejercicios01;
+import Clase03.ContadorDeLetras;
 
 public class Main {
     public static void main(String[] args) {
-        Ejercicios01.respuestas(5,14, false);
-        Ejercicios01.categoria(130000,30,3000);
+//        Ejercicios01.respuestas(5,14, false);
+//        Ejercicios01.categoria(130000,30,3000);
+        ContadorDeLetras.contador('s', "pelapapas");
     }
 }
