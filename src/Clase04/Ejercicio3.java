14ced2e59afd70621c1ccd857d5d52a5b538ab85
Pablo Ezequiel Balanzino <pbalanzino@gmail.com>
2023-03-24 14:59:11 -0300

    Ejercicio3

---
+++ src/Clase04/Ejercicio3.java
@@ -0,0 +1,12 @@
+package Clase04;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+      /* Tome el ejercicio B de la clase 3 y que por parámetro se pueda elegir si es una
+        codificación o decodificación, el valor desplazado, y 2 archivos, uno para la entrada y
+        otro para la salida. Que por pantalla solo indique si terminó o no correctamente, los
+        resultados deben estar en el archivo de salida.*/
+
+
+    }
+}
