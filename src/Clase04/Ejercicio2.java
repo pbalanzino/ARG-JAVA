52519337302e893890a467b7d11faa56959d917d
Pablo Ezequiel Balanzino <pbalanzino@gmail.com>
2023-03-24 14:05:56 -0300

    Ejercicio2.a completo

--- src/Clase04/Ejercicio2.java
+++ src/Clase04/Ejercicio2.java
@@ -2,19 +2,42 @@ package Clase04;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import static java.lang.Integer.parseInt;
+import java.nio.file.Path;
+import java.util.Scanner;
 import static java.nio.file.Paths.*;
 
 public class Ejercicio2 {
     public static void main(String[] args) throws IOException {
         String RUTA = "C:\\Users\\pablo\\OneDrive\\Documentos\\dev\\ARG-JAVA\\src\\Clase04\\numeros.txt";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ejercicio 2");
+        Path path = get(RUTA);
         int total = 0;
-            for(String linea : Files.readAllLines(get(RUTA))){
+        for(String linea : Files.readAllLines(path)){
+            total += Integer.parseInt(linea);
+        }
+        System.out.println("\nLos numeros del archivo son: " + Files.readAllLines(path));
+        System.out.println("El total es: " + total);
+        System.out.println("\nEjercicio 2.a\n");
+        System.out.print("Escriba \"s\" para sumar o \"m\" para multiplicar: ");
+        String respuesta = sc.next();
+        total = 0;
+        if (respuesta.equals("s")) {
+            for(String linea : Files.readAllLines(path)){
                 total += Integer.parseInt(linea);
             }
-        System.out.println("Los numeros son: " + Files.readAllLines(get(RUTA)).toString());
-        System.out.println("El total es: " + total);
+            System.out.println("\nLos numeros del archivo son: " + Files.readAllLines(path));
+            System.out.println("El total es: " + total);
+        } else if (respuesta.equals("m")) {
+            total = 1;
+            for(String linea : Files.readAllLines(path)){
+                total *= Integer.parseInt(linea);
+            }
+            System.out.println("\nLos numeros del archivo son: " + Files.readAllLines(path));
+            System.out.println("El total es: " + total);
+        } else {
+            System.out.println("No se reconoce la respuesta");
+        }
+
     }
 }
