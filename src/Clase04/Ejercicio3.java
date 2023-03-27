d8d59a69d10ebf85a1a590ecde8874c8f616dff1
Pablo Ezequiel Balanzino <pbalanzino@gmail.com>
2023-03-27 19:08:49 -0300

    Comentarios y correccion de nombre de variables

--- src/Clase04/Ejercicio3.java
+++ src/Clase04/Ejercicio3.java
@@ -2,6 +2,7 @@ package Clase04;
 
 import Clase03.Criptografo;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,43 +10,46 @@ import java.util.Arrays;
 
 import static java.nio.file.Paths.*;
 
-public class Ejercicio3 extends Criptografo {
-    public static char[] codificar1(String diccionario, String texto, int posiciones) {
-        char[] diccionarioArray = new char[diccionario.length()];
+public class Ejercicio3 {
+    /* Tuve que copiar el método codificar y decodificar de la clase Criptografo
+    para poder usarlos en este ejercicio. */
+
+    /* args => decodificar 1 .\src\Clase04\entrada.txt salida.txt */
+    /* args => codificar 1 .\src\Clase04\entrada.txt salida.txt */
+
+    public static String codificar(String diccionario, String texto, int posiciones) {
+        char[] alfabeto = new char[diccionario.length()];
         for (int i = 0; i < diccionario.length(); i++) {
-            diccionarioArray[i] = diccionario.charAt(i);
+            alfabeto[i] = diccionario.charAt(i);
         }
-        String textoLower = texto.toLowerCase();
-        char letra[] = new char[textoLower.length()];
-        for (int i = 0; i < textoLower.length(); i++) {
-            char caracter = textoLower.charAt(i);
-            for (int j = 0; j < diccionarioArray.length; j++) {
-                if (caracter == diccionarioArray[j]) {
-                    if (diccionarioArray[j + posiciones] < diccionarioArray.length)
-                        letra[i] = diccionarioArray[j + posiciones];
-                    else letra[i] = diccionarioArray[0];
+        char letras[] = new char[texto.toLowerCase().length()];
+        for (int i = 0; i < texto.length(); i++) {
+            char letra = texto.toLowerCase().charAt(i);
+            for (int j = 0; j < alfabeto.length; j++) {
+                if (letra == alfabeto[j]) {
+                    letras[i] = alfabeto[j + posiciones];
                 }
             }
         }
-        return letra;
+        return String.valueOf(letras);
     }
-    public static String decodificar1(String diccionario,String textoCodificado,int posiciones){
-        String textoLower = textoCodificado.toLowerCase();
-        char[] diccionarioArray = new char[diccionario.length()];
+
+    public static String decodificar(String diccionario,String textoCodificado,int posiciones){
+        char[] alfabeto = new char[diccionario.length()];
         for (int i = 0; i < diccionario.length(); i++) {
-            diccionarioArray[i] = diccionario.charAt(i);
+            alfabeto[i] = diccionario.charAt(i);
         }
-        char letra[] = new char[textoLower.length()];
-        for (int i = 0; i < textoLower.length(); i++) {
-            char caracter = textoLower.charAt(i);
-            for (int j = 0; j < diccionarioArray.length; j++) {
-                if (caracter == diccionarioArray[j]) {
-                    if (caracter != ' ') letra[i] = diccionarioArray[j - posiciones];
-                    else letra[i] = diccionarioArray[diccionarioArray.length - posiciones];
+        char letras[] = new char[textoCodificado.toLowerCase().length()];
+        for (int i = 0; i < textoCodificado.length(); i++) {
+            char letra = textoCodificado.toLowerCase().charAt(i);
+            for (int j = 0; j < alfabeto.length; j++) {
+                if (letra == alfabeto[j]) {
+                    if (letra != ' ') letras[i] = alfabeto[j - posiciones];
+                    else letras[i] = alfabeto[alfabeto.length - posiciones];
                 }
             }
         }
-        return String.valueOf(letra);
+        return String.valueOf(letras);
     }
 
     public static void main(String[] args) throws IOException {
@@ -53,27 +57,24 @@ public class Ejercicio3 extends Criptografo {
         codificación o decodificación, el valor desplazado, y 2 archivos, uno para la entrada y
         otro para la salida. Que por pantalla solo indique si terminó o no correctamente, los
         resultados deben estar en el archivo de salida.*/
-        Path path = get(args[2]);
-        String palabra = Files.readAllLines(path).get(0);
+        File file = new File(args[2]);
+        Path path = get(file.getAbsolutePath());
+        String texto = Files.readAllLines(path).get(0);
         String alfabeto = " abcdefghijklmnopqrstuvwxyz";
-        Path nuevoArchivo = get("./src/Clase04/salida.txt");
-        System.out.println(palabra);
-        if(Files.exists(nuevoArchivo))
-            Files.delete(nuevoArchivo);
-        Files.createFile(nuevoArchivo);
+        Path archivoSalida = get("./src/Clase04/salida.txt");
+//        System.out.println("\nEl texto de entrada es: " + texto);
+        if(Files.exists(archivoSalida))
+            Files.delete(archivoSalida);
+        Files.createFile(archivoSalida);
         if(args[0].equals("codificar")){
-            char[] texto = codificar1(alfabeto, "auto", 1);
-            System.out.println(Arrays.toString(texto));
-            String texto2 = "chau";
-            Files.write(nuevoArchivo, texto2.getBytes());
+            String codificacion = codificar(alfabeto, texto, Integer.parseInt(args[1]));
+            Files.write(archivoSalida, codificacion.getBytes());
             System.out.println("\nSe realizó la codificación.");
-        } /*else if(args[0].equals("decodificar")){
-            String texto = decodificar1(alfabeto, "auto", Integer.parseInt(args[1]));
-            System.out.println(texto);
-            byte[] strToBytes = texto.getBytes();
-            Files.write(nuevoArchivo, strToBytes);
+        } else if(args[0].equals("decodificar")){
+            String decodificacion = decodificar(alfabeto, texto, Integer.parseInt(args[1]));
+            Files.write(archivoSalida, decodificacion.getBytes());
             System.out.println("\nSe realizó la decodificación.");
-        }*/ else {
+        } else {
             System.out.println("\nNo se pudo realizar la operación");
         }
     }
